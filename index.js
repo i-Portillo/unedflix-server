102d50c1ad4d9af3752719312736c7ccb17f6903
iPortillo <ismaelpa91@gmail.com>
2022-05-03 19:23:42 +0200

    First commit. Basic server setup.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
