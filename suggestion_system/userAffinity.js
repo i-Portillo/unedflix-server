007d31c484a47bf3adbf57e084a568a517b53196
iPortillo <ismaelpa91@gmail.com>
2022-06-10 13:57:56 +0200

    Adjust user affinity. Final

--- suggestion_system/userAffinity.js
+++ suggestion_system/userAffinity.js
@@ -1,11 +1,4 @@
-const mongo = require('../db/mongo');
-
-const async = require('async');
-
-const MediaReview = require('../models/mediaReview');
-const User = require('../models/user')
-
-mongo.connect();
+import MediaReview from '../models/mediaReview.js'
 
 const getUserSimilarity = (userReviews, otherReviews) => {
 
@@ -26,40 +19,9 @@ const getUserSimilarity = (userReviews, otherReviews) => {
   }
 
   return similarity;
-
-  // const results = {
-  //   user: other._id,
-  //   match: 0,
-  // };
-
-  // let affinity = 0;
-  
-  // userReviews.forEach(userReview => {
-
-  //   const found = otherReviews.find(otherReview => otherReview.media.equals(userReview.media));
-  //   if (found) {
-  //     results.match += 1;
-  //     if (userReview.feedback === found.feedback) affinity++;
-  //     results.feedback = found.feedback;
-  //   }
-  // });
-
-  // if (results.match) results.affinity = affinity / results.match;
-
-  // return results;
 }
 
-// const getAffinity = (userReviews, other, similarityTable) => {
-
-//   if (!similarityTable[other._id]) {
-//     similarityTable[other._id] = getUserSimilarity(userReviews, other.media_reviews);
-//   }
-
-//   return 
-
-// }
-
-const evaluateMedia = async (mediaId, userReviews, similarityTable) => {
+export const evaluateMedia = async (mediaId, userReviews, similarityTable) => {
 
   const affinityThreshold = 0.6;
 
@@ -72,68 +34,33 @@ const evaluateMedia = async (mediaId, userReviews, similarityTable) => {
         path: 'media_reviews',
         model: 'MediaReview'
       }
-    })
+    });
 
-    const otherUsers = reviews.map( review => review.user);
+    const otherUsers = reviews.map( review => {
+      const otherUser = review.user;
+      otherUser.feedback = review.feedback;
+      return otherUser;
+    });
+
+    let sum = 0;
+    let count = 0;
 
     otherUsers.forEach( otherUser => {
       if (!similarityTable[otherUser.email]) {
         similarityTable[otherUser.email] = getUserSimilarity(userReviews, otherUser.media_reviews);
       }
-    })
+      if (similarityTable[otherUser.email] >= affinityThreshold) {
+        count++;
+        sum += (otherUser.feedback === true ? similarityTable[otherUser.email] : -similarityTable[otherUser.email])
+      }
+    });
 
-    // console.log(similarityTable.length);
+    const evaluation = ((count === 0 ? 0  : (sum / count)) + 1) * 50; // Avoid division by 0 and standarization
 
-    return 100;
-
-    // // For every review that media has compare the affinity with the user
-    // let userAffinities = await Promise.all(
-    //   reviews.map( async review => {
-
-
-    //     if (!similarityTable[review.user._id]) {
-
-    //     }
-        
-    //     const other = await User.findById(review.user)
-    //     .populate({
-    //       path: 'media_reviews'
-    //     });
-    //     return compareUsers(userReviews, other);
-    //   })
-    // )
-
-    // userAffinities = userAffinities.filter( userAffinity => userAffinity.affinity >= affinityThreshold)
-
-    // if (userAffinities.length === 0) return 50;  // If not enough affinities => 50/50
-
-    // const positiveFeedback = userAffinities.reduce( (prev, current) => {
-    //   return current.feedback ? prev + 1 : prev; // Counts the number of positive feedbacks
-    // }, 0)
-
-    // return 100 * positiveFeedback / userAffinities.length;
+    return evaluation;
 
   } catch(err) {
     console.log(err);
   }
 
 }
-
-module.exports = { evaluateMedia };
-
-// DEBUG
-
-// const debug = async () => {
-
-//   try {
-//     const user = await User.findOne({})
-//     .populate({
-//       path: 'media_reviews'
-//     });
-//     console.log(await evaluateMedia({_id: '6295086043b62f47d0bf3479'}, user.media_reviews));
-//   } catch (err) {
-//     console.log(err);
-//   }  
-// }
-
-// debug();
